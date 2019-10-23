ce010b8d60d70284c0b0835ef8fe1a65a6520bcc
abengengineer <53614344+abengengineer@users.noreply.github.com>
2019-10-23 20:12:48 +0800

    旅游攻略增加游记

--- store/post.js
+++ store/post.js
@@ -1,4 +1,16 @@
+export const state = () => {
+    return {
+        
+draft: []
+    }
+}
+
 // 存放的是同步修改state的方法
-export const mutation = {
-    
+export const mutations = {
+    setdraft(state,data){
+        state.draft.push(data)
+    },
+    deldraft(state,index){
+        state.draft.splice(index,1)
+    }
 }

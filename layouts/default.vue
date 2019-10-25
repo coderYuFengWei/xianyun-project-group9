f05bfd124da7712d22e91e7c639e91e9ed9bcac0
coderYuFengWei <53595007+coderYuFengWei@users.noreply.github.com>
2019-10-25 11:31:55 +0800

    更新数据

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,6 +14,7 @@
 <script>
 import Header from "@/components/header"
 import Footer from "@/components/footer"
+import iconFont from "@/assets/fonts/iconfont.css"
 
 export default {
   components: {
